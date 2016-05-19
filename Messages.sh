e1306318fca468eed575a0c8038c966698312858
Rohan Garg <rohan@garg.io>
2016-05-19 15:46:58 +0200

    Drop everything except the packaging

--- Messages.sh
+++
@@ -1,4 +0,0 @@
-#! /usr/bin/env bash
-$EXTRACTRC `find . -name "*.ui" | grep -v '/tests/' | grep -v '/tools/'` >> rc.cpp || exit 11
-$XGETTEXT `find . -name "*.cpp" | grep -v '/tests/' | grep -v '/tools/'` -o $podir/ktp-common-internals.pot
-rm -f rc.cpp
